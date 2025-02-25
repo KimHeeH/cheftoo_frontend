2f5bec9d2a941d53ed7866909433391ac1afaca1
KimHeeSoo <khstoday@naver.com>
2025-02-25 19:14:14 +0900

    commit css

--- src/Component/SearchContainer/SearchContainer.jsx
+++ src/Component/SearchContainer/SearchContainer.jsx
@@ -5,6 +5,7 @@ import recipeIcon from "../img/Recipe.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./SearchContainer.style.css";
 import { useNavigate } from "react-router-dom";
+import searchIcon from "./icon/searchIcon.svg";
 const SearchContainer = () => {
   const navigate = useNavigate();
   const handleLoginPage = () => {
@@ -16,17 +17,10 @@ const SearchContainer = () => {
         <Row className="align-items-center">
           <Col lg="12" className="searchContainer-login-container">
             <div
-              className="searchContainer-login-button"
-              onClick={handleLoginPage}
-            >
-              로그인
-            </div>
-            <div style={{ color: "#b1b1b1", marginRight: "15px" }}> | </div>
-            <div
               className="searchContainer-signUp-button"
               onClick={handleLoginPage}
             >
-              회원가입
+              로그인 / 회원가입
             </div>
           </Col>
           <Col lg="6" className="logo-container">
@@ -42,7 +36,11 @@ const SearchContainer = () => {
             xs="12"
             className="search-input-container"
           >
-            <input className="input" placeholder="요리를 검색해보세요" />
+            <input
+              className="input"
+              placeholder="만들고싶은 요리를 검색해보세요"
+            />
+            <img className="searchIcon" width="20" src={searchIcon} />
           </Col>
         </Row>
       </Container>
